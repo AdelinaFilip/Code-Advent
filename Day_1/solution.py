f7794d06fdc9ce602b1725d37d86e64e5edd1e33
Adelina Filip <adelinaf21@gmail.com>
2021-12-30 20:59:16 +0200

    day 1 part 2

--- Day_1/solution.py
+++ Day_1/solution.py
@@ -12,4 +12,20 @@ for i, measurement in enumerate(lines[:(len(lines) - 2)]):
     if measurement < lines[i+1]:
         count += 1
 
-print(count)
+print(f"Part 1: {count}")
+
+# Part 2
+window_count = 0
+
+if len(lines) < 4:
+    window_count = 0
+
+window_sum = sum(lines[:3])
+for i in range(3, len(lines), 1):
+    next_window_sum = window_sum - lines[i - 3] + lines[i]
+    if next_window_sum > window_sum:
+        window_count += 1
+    
+    window_sum = next_window_sum
+
+print(f"Part 2: {window_count}")
